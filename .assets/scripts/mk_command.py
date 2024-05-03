222a78bf38a6931524ac216b0707608c4aaec80b
CrystalStudios <103898803+CRYSTAL7271@users.noreply.github.com>
2024-05-03 22:18:48 +0200

    Update mk_command.py

--- .assets/scripts/mk_command.py
+++ .assets/scripts/mk_command.py
@@ -1,7 +1,7 @@
 import os
 os.chdir("./../usr/bin")
 f=open("ci-terminal.sh", "w")
-f.write("cd\npython CoolIntro/scripts/terminal.py")
+f.write("cd\npython CoolIntro/.assets/scripts/terminal.py")
 f.close()
 os.system("mv ci-terminal.sh ci-terminal")
 os.system("chmod +x ci-terminal")
