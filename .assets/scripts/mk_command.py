ebd60bf019658dc003cc510d6c1dbd710efde3bc
CrystalStudios <103898803+CRYSTAL7271@users.noreply.github.com>
2024-05-03 22:05:49 +0200

    Delete .assets/scripts directory

--- .assets/scripts/mk_command.py
+++
@@ -1,8 +0,0 @@
-import os
-os.chdir("./../usr/bin")
-f=open("ci-terminal.sh", "w")
-f.write("cd\npython CoolIntro/scripts/terminal.py")
-f.close()
-os.system("mv ci-terminal.sh ci-terminal")
-os.system("chmod +x ci-terminal")
-print("Python script finished.")
