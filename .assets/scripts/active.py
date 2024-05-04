34554ea8f62e30cdda960d05cb55a3a4577acc2f
CrystalStudios <103898803+CRYSTAL7271@users.noreply.github.com>
2024-05-04 04:46:44 -0700

    Add files via upload

---
+++ .assets/scripts/active.py
@@ -0,0 +1,6 @@
+import os
+
+f=open('termux-login.sh', 'w')
+f.write('python CoolIntro/.assets/scripts/main_script.py')
+f.close()
+
---
+++ .assets/scripts/deactivate.py
@@ -0,0 +1,6 @@
+import os
+
+f=open('termux-login.sh', 'w')
+f.write('# write all shell commands before input')
+f.close()
+
